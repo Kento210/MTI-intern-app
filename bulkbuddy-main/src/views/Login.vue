843db528df5492cf2f1b7a7dbe6c47d0a0816ed6
futaba3 <nyokinyoki41aya@gmail.com>
2023-08-24 05:33:00 +0000

    add: 新規登録画面の入力情報を入れる見た目を完成

--- bulkbuddy-main/src/views/Login.vue
+++ bulkbuddy-main/src/views/Login.vue
@@ -22,32 +22,70 @@
           <h3 class="ui centered header title-text">
             {{ toggledTitleText }} <br> {{ toggledSubTitleText }}
           </h3>
-          
+
+          <div class="field" v-if="!isLogin">
+            <label>目標体重</label>
+            <input v-model="user.nickname" type="number" min="30" placeholder="目標体重の数字を入力" />
+            <!--右側にkgの表示をつけることもできるが高さの修正方法が不明-->
+            <!--<div class="ui right labeled input">-->
+            <!--  <input type="text" placeholder="Enter weight..">-->
+            <!--  <div class="ui basic label">-->
+            <!--    kg-->
+            <!--  </div>-->
+            <!--</div>-->
+          </div>
+
           <div class="field">
-            <p>ユーザー名</p>
+            <label>ユーザー名</label>
             <input v-model="user.userId" type="text" placeholder="ユーザー名を入力" />
           </div>
 
-          
+
           <div class="field">
-            <p>パスワード</p>
+            <label>パスワード</label>
             <input v-model="user.password" type="password" placeholder="パスワードを入力" />
           </div>
 
           <div class="field" v-if="!isLogin">
-            <p>ユーザー名</p>
-            <i class="tag icon"></i>
-            <input v-model="user.nickname" type="text" placeholder="Nickname" />
-          </div>
-
-          <div class="field" v-if="!isLogin">
-            <div class="ui left icon input">
-              <i class="calendar icon"></i>
-              <input v-model.number="user.age" type="number" min="0" placeholder="Age" />
+            <label>性別（正確なカロリー計算のために使用します）</label>
+            <div class="fields">
+              <label>
+                <input type="radio" v-model="selectedOption" name="options" value="male" />
+                男性
+              </label>
+              <label>
+                <input type="radio" v-model="selectedOption" name="options" value="female" />
+                女性
+              </label>
             </div>
           </div>
 
-          
+          <div class="field" v-if="!isLogin">
+            <label>生年月日</label>
+            <div class="fields">
+              <div class="field">
+                <input v-model="year" type="number" min="1950" max="2023" placeholder="年" />
+              </div>
+              <div class="field">
+                <input v-model="month" type="number" min="1" max="12" placeholder="月" />
+              </div>
+              <div class="field">
+                <input v-model="day" type="number" min="1" max="31" placeholder="日" />
+              </div>
+            </div>
+          </div>
+
+          <div class="fields" v-if="!isLogin">
+            <div class="field">
+              <label>身長 (cm)</label>
+              <input v-model.number="height" type="number" min="100" max="250" placeholder="身長" />
+            </div>
+            <div class="field">
+              <label>体重 (kg)</label>
+              <input v-model.number="weight" type="number" min="30" placeholder="体重" />
+            </div>
+          </div>
+
           <div class="ui centered grid">
             <div class="center aligned column">
               <button class="ui button orange-button" :disabled="isButtonDisabled" type="submit">{{ submitBtnText }}</button>
@@ -61,7 +99,7 @@
           <button @click="toggleMode" class="ui button grey-button" type="submit">{{ toggledBtnText }}</button>
         </div>
       </div>
-  
+
     </div>
   </div>
 </template>
@@ -88,6 +126,7 @@
           password: null,
           nickname: null,
           age: null
+          // 男性は0女性1
         },
         errorMsg: '', // 発展課題のエラーメッセージ用
         isCallingApi: false // 発展課題のローディング表示用
@@ -104,15 +143,15 @@
           !userId || !password :
           !userId || !password || !nickname || !age;
       },
-      
+
       toggledTitleText() {
         return this.isLogin ? 'ログイン' : '目標設定には新規登録が必要です'
       },
-      
+
       toggledSubTitleText() {
         return this.isLogin ? '' : 'あなたについて教えてください'
       },
-      
+
       submitBtnText() {
         return this.isLogin ? 'ログイン' : '新規登録'
       },
@@ -180,19 +219,21 @@
 
 <style scoped>
   /* このコンポーネントだけに適用するCSSはここに記述する */
-.orange-button {
-  background-color: #F68712;
-  color: white;
-  width: 25%;
-  margin: 20px;
-}
-.grey-button {
-  background-color: #333;
-  color: white;
-  width: 24%;
-  margin: 20px;
-}
-.title-text {
-  padding: 20px;
-}
+  .orange-button {
+    background-color: #F68712;
+    color: white;
+    width: 25%;
+    margin: 20px;
+  }
+
+  .grey-button {
+    background-color: #333;
+    color: white;
+    width: 24%;
+    margin: 20px;
+  }
+
+  .title-text {
+    padding: 20px;
+  }
 </style>
